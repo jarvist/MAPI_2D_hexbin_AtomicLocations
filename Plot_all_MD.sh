94edd2680e1cc439d21bc5aad553f6dcfcaf3ce1
Jarvist Moore Frost <jarvist@gmail.com>
2016-04-27 15:44:00 +0100

    Whoops - wrong var-name; wasn't regenerating data in loop!

--- Plot_all_MD.sh
+++ Plot_all_MD.sh
@@ -7,9 +7,9 @@ do
  # Calculate Trace of POSCAR unit cell thingy at the top, divided by two (size of unit cell, assuming 2x2x2 supercell)
  unitcell=` cat "${prefix}.POSCAR" | awk 'BEGIN{v=0.0} NR==3{v=v+$1} NR==4{v=v+$2} NR==5{v=v+$3} END{print (v/3)/2}' `
  
- grep "Pb\|Sn\|Ge" "${f}" > HeavyMetal.dat
- grep "I\|Br\|Cl" "${f}" > Halogen.dat
- grep " C " "${f}" > C.dat
+ grep "Pb\|Sn\|Ge" "${i}" > HeavyMetal.dat
+ grep "I\|Br\|Cl" "${i}" > Halogen.dat
+ grep " C \|Cs" "${i}" > C.dat
 
 # unitcell optionally passed to the AWK PBC program to chop PBCs at a different place
  cat HeavyMetal.dat | ./XYZ_to_Fractional_PBCs.awk "unitcell=${unitcell}" > HeavyMetal_symm.dat
